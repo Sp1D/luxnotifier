996cf23b9878543ba2768801f3177256d0462f0b
Sp1D <buzzband@gmail.com>
2018-03-03 17:50:49 +0100

    let's use embedded hsqldb again

--- luxmed-web/src/main/java/sp1d/luxnotifier/web/DataSourceConfiguration.java
+++ luxmed-web/src/main/java/sp1d/luxnotifier/web/DataSourceConfiguration.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
@@ -28,28 +27,10 @@ public class DataSourceConfiguration {
 
     @Bean(name = "dataSource")
     public DataSource dataSource() {
-        String user = environment.getProperty("db.user");
-        String password = environment.getProperty("db.password");
-        String url = environment.getProperty("db.url");
-        String driver = environment.getProperty("db.driver");
-
-        LOG.info("Connecting to {}", url);
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUsername(user);
-        dataSource.setPassword(password);
-        dataSource.setUrl(url);
-        dataSource.setDriverClassName(driver);
-
-        return dataSource;
-    }
-
-    @Bean(name = "dataSource")
-    @Profile("dev")
-    public DataSource devDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUsername("SA");
         dataSource.setPassword("");
-        dataSource.setUrl("jdbc:hsqldb:file:db/sqlnotifier_db");
+        dataSource.setUrl("jdbc:hsqldb:file:/mnt/data/luxnotifier_db");
         dataSource.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
         initDatabase(dataSource);
 
